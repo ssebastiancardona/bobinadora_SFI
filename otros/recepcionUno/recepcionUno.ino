122c8071e84599698ce828184eaa1ad6ab58bc07
ssebastiancardona <ss3bastiancardona@gmail.com>
2023-08-10 17:34:42 -0500

    ready_4_test

--- otros/recepcionUno/recepcionUno.ino
+++ otros/recepcionUno/recepcionUno.ino
@@ -1,35 +1,54 @@
 struct servoMoveMessage
 {
-    int servoNum;
+    float servoNum;
 };
 
 struct servoMoveMessage message;
 int valorEntrada;
+bool activo;
 void recieveStructure(byte *structurePointer, int structureLength)
 {
     if (Serial.available() < sizeof(message))
         return;
     Serial.readBytes(structurePointer, structureLength);
-    Serial.println(valorEntrada);
+    // Serial.println(valorEntrada);
 }
 
 void setup()
 {
     Serial.begin(9600);
+    // pinMode(LED_BUILTIN, OUTPUT);
+    //  pinMode(13, OUTPUT);
+    //  digitalWrite(13, LOW);
+    pinMode(11, OUTPUT);
+    digitalWrite(11, LOW);
+    pinMode(4, INPUT_PULLUP);
     recieveStructure((byte *)&message, sizeof(message));
-    pinMode(13, OUTPUT);
-    digitalWrite(13, LOW);
 }
 
 void loop()
 {
     valorEntrada = message.servoNum;
+    int aceleracion = valorEntrada * 15;
+
     if (Serial.available())
     {
         recieveStructure((byte *)&message, sizeof(message));
     }
-    digitalWrite(LED_BUILTIN, HIGH); // turn the LED on (HIGH is the voltage level)
-    delay(valorEntrada);             // wait for a second
-    digitalWrite(LED_BUILTIN, LOW);  // turn the LED off by making the voltage LOW
-    delay(valorEntrada);             // wait for a second
+
+    while (digitalRead(4) == LOW)
+    {
+        while (digitalRead(4) == LOW && aceleracion > valorEntrada)
+        {
+            digitalWrite(11, HIGH);         // turn the LED on (HIGH is the voltage level)
+            delayMicroseconds(aceleracion); // wait for a second
+            digitalWrite(11, LOW);          // turn the LED off by making the voltage LOW
+            delayMicroseconds(aceleracion); // wait for a second
+            aceleracion--;
+        }
+        digitalWrite(11, HIGH);          // turn the LED on (HIGH is the voltage level)
+        delayMicroseconds(valorEntrada); // wait for a second
+        digitalWrite(11, LOW);           // turn the LED off by making the voltage LOW
+        delayMicroseconds(valorEntrada); // wait for a second
+    }
 }
